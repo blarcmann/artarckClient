7e2784344950cbf330039110160ab9becb7b45d5
yemiOdetola <yemiotola@outlook.com>
2019-03-03 16:48:09 +0100

    feat: reviews check/post/validation/rating

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -11,6 +11,12 @@ import { RestApiService } from '../rest-api.service';
 export class ProductComponent implements OnInit {
   product: any;
   baseUrl = 'http://localhost:3000/api';
+  myReview = {
+    title: '',
+    description: '',
+    rating: 0
+  }
+  btnDisabled = false;
   constructor(
     private data: DataService,
     private activatedRoute: ActivatedRoute,
@@ -31,8 +37,34 @@ export class ProductComponent implements OnInit {
     })
   }
 
+  async postReview() {
+    this.btnDisabled = true;
+    try {
+      const data = this.rest.post(`${this.baseUrl}/review`, {
+        productId: this.product._id,
+        title: this.myReview.title,
+        description: this.myReview.description,
+        rating: this.myReview.rating
+      });
+      data['success']
+        ? this.data.success(data['message'])
+        : this.data.error(data['message'])
+    } catch (error) {
+      this.data.error(error['message'])
+    }
+    this.btnDisabled = false;
+  }
+
   addToCart() {
     console.log('Added toCard!');
   }
 
 }
+
+
+
+
+// M4HRMABA2K == application id
+// 5d287e3004545fb39448354d585deb65  == searchonly API key
+// 179c84f716ec086ae21c9ee084a5d2fd  == admin API key
+// fa84a72395154a67a0e2acf9c14e2090  == monitoring API key
