033594258b52aaca7577d96fece6775aac82e2e1
yemiOdetola <yemiotola@outlook.com>
2019-03-21 21:29:33 +0100

    feat: homepage full

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,22 +9,46 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
   products: any;
-  baseUrl: 'http://localhost:3000/api'
+  categories: any;
+  baseUrl: 'http://localhost:3000/api';
   constructor(
     private rest: RestApiService,
     private data: DataService
-  ) {}
+  ) { }
 
   async ngOnInit() {
     try {
       const data = await this.rest.get(`http://localhost:3000/api/products`);
       data['success']
-      ? (this.products = data['products'])
-      : this.data.error('Could not fetch Products');
-
+        ? (this.products = data['products'])
+        : this.data.error('Could not fetch Products');
+        console.log(data);
+    } catch (error) {
+      this.data.error(error['message']);
+    }
+    try {
+      const data = await this.rest.get(`http://localhost:3000/api/categories`);
+      console.log(data);
+      data['success']
+        ? (this.categories = data['categories'])
+        : this.data.error(data['message']);
     } catch (error) {
       this.data.error(error['message']);
     }
   }
+
+  // fetchCategories() {
+  //   // try {
+  //     const data = this.rest.get(`http://localhost:3000/api/categories`);
+  //     console.log(data);
+  //     data['success']
+  //       ? (this.categories = data['categories'])
+  //       : this.data.error('An error occured from our end, Please retry');
+  //   // } catch (error) {
+  //   //   this.data.error(error['message']);
+  //   // }
+  // }
+
+
 
 }
